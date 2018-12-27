8d1ebbbfd6c462a7bf708c591fab874045213753
Yamagn <syankenpon@gmail.com>
2018-12-27 11:12:17 +0900

    using v-html

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message:'Hello Vue.js!'
+        message:'<h1>Hello</h1>' +  '<span><a href="https://jp.vuejs.org/">Vue.js!</a></span>'
     }
 });
