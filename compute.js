c35461d7964bbaaeb584d3beb6ef46ea1c480ead
Yamagn <syankenpon@gmail.com>
2019-01-02 19:38:50 +0900

    イベント系の追加

---
+++ compute.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: '#app',
+    data: {
+        email: 'Yamada@example.com'
+    },
+
+    computed: {
+        localEmail: function() {
+            return this.email.split('@')[0].toLowerCase();
+        }
+    }
+});
